9ab4b0c5457fdb7be44b06eb2caafc0685f93be3
Acrossover <180825556@qq.com>
2023-05-18 17:03:45 +0800

    feat: 新建项目vite框架

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>我是App</h1>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: Acrossover 180825556@qq.com
+ * @Date: 2023-05-18 15:08:59
+ * @Description: My good baby!
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
